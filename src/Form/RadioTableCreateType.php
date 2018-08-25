80049f38a28316f78c969962ec8d6453717e085c
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2018-08-25 16:47:01 +0200

    Introduce RadioTableCreateType form class.
    Refactor RadioTableSettingsType class — now it extends RadioTableCreateType class.

---
+++ src/Form/RadioTableCreateType.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\RadioTable;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RadioTableCreateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', null, [
+                'label' => 'Nazwa wykazu',
+            ])
+            ->add('useKhz', ChoiceType::class, [
+                'label' => 'Jednostka częstotliwości',
+                'choices' => [
+                    'kHz' => 1,
+                    'MHz' => 0,
+                ]
+            ])
+            ->add('sorting', ChoiceType::class, [
+                'label' => 'Domyślne sortowanie',
+                'choices' => [
+                    'częstotliwość'      => RadioTable::SORTING_FREQUENCY,
+                    'nazwa'              => RadioTable::SORTING_NAME,
+                    'numer w odbiorniku' => RadioTable::SORTING_PRIVATE_NUMBER,
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Opis wykazu',
+            ])
+            ->add('status', ChoiceType::class, [
+                'expanded' => true,
+                'choices' => [
+                    'Publiczny — wykaz może zobaczyć każdy'     => RadioTable::STATUS_PUBLIC,
+                    'Niepubliczny — wykaz mogą zobaczyć jedynie osoby, które otrzymają odnośnik'
+                        => RadioTable::STATUS_UNLISTED,
+                    'Prywatny — wykaz możesz zobaczyć tylko ty' => RadioTable::STATUS_PRIVATE,
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => RadioTable::class,
+        ]);
+    }
+}
--- src/Form/RadioTableSettingsType.php
+++ src/Form/RadioTableSettingsType.php
@@ -4,61 +4,24 @@ namespace App\Form;
 
 use App\Entity\RadioTable;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class RadioTableSettingsType extends AbstractType
+class RadioTableSettingsType extends RadioTableCreateType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        parent::buildForm($builder, $options);
+
         $builder
-            ->add('name', null, [
-                'label' => 'Nazwa wykazu',
-            ])
-            ->add('useKhz', ChoiceType::class, [
-                'label' => 'Jednostka częstotliwości',
-                'choices' => [
-                    'kHz' => 1,
-                    'MHz' => 0,
-                ]
-            ])
-            ->add('sorting', ChoiceType::class, [
-                'label' => 'Domyślne sortowanie',
-                'choices' => [
-                    'częstotliwość'      => RadioTable::SORTING_FREQUENCY,
-                    'nazwa'              => RadioTable::SORTING_NAME,
-                    'numer w odbiorniku' => RadioTable::SORTING_PRIVATE_NUMBER,
-                ],
-            ])
-            ->add('description', TextareaType::class, [
-                'label' => 'Opis wykazu',
-            ])
-            ->add('status', ChoiceType::class, [
-                'expanded' => true,
-                'choices' => [
-                    'Publiczny — wykaz może zobaczyć każdy'     => RadioTable::STATUS_PUBLIC,
-                    'Niepubliczny — wykaz mogą zobaczyć jedynie osoby, które otrzymają odnośnik'
-                        => RadioTable::STATUS_UNLISTED,
-                    'Prywatny — wykaz możesz zobaczyć tylko ty' => RadioTable::STATUS_PRIVATE,
-                ],
-            ])
             ->add('columns', null, [
                 'entry_type' => IntegerType::class,
                 // Label for each children is defined in finishView().
             ])
         ;
-    }
-
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => RadioTable::class,
-        ]);
     }
 
     public function finishView(FormView $view, FormInterface $form, array $options)
