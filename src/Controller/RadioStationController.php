397733eabbf3eecc863486a32217c58b2dc54b6b
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2018-08-26 00:47:27 +0200

    Implement RadioStationController::edit.

--- src/Controller/RadioStationController.php
+++ src/Controller/RadioStationController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\RadioStation;
+use App\Form\RadioStationEditType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RadioStationController extends AbstractController
@@ -16,11 +20,20 @@ class RadioStationController extends AbstractController
     }
 
     /**
-     * @Route("/edytuj-stacje", name="radiostation_edit")
+     * @Route("/edytuj-stacje/{id}", name="radiostation_edit")
      */
-    public function edit()
+    public function edit(RadioStation $radioStation, Request $request, EntityManagerInterface $entityManager)
     {
-        return $this->render('radiostation/edit.html.twig');
+        $form = $this->createForm(RadioStationEditType::class, $radioStation);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+        }
+
+        return $this->render('radiostation/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 
     /**
