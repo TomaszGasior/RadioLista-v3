b447353f9048b66aae064b6f3172217cd07ea9f6
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2018-08-27 00:12:32 +0200

    Introduce SessionLogInType form.

---
+++ src/Form/SessionLogInType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SessionLogInType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'label' => 'Nazwa użytkownika',
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Hasło',
+            ])
+            ->add('rememberMe', CheckboxType::class, [
+                'label' => 'Zapamiętaj mnie',
+                'required' => false,
+            ])
+        ;
+    }
+}
