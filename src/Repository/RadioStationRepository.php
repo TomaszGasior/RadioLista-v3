54596f8d32d8503e457d56d355ba1a5126c2ec46
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2018-09-02 00:58:15 +0200

    Introduce RadioStationRepository::findForRadioTable.
    Sorting by private number needs some work.

--- src/Repository/RadioStationRepository.php
+++ src/Repository/RadioStationRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\RadioStation;
+use App\Entity\RadioTable;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
 
@@ -17,6 +18,40 @@ class RadioStationRepository extends ServiceEntityRepository
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, RadioStation::class);
+    }
+
+    public function findForRadioTable(RadioTable $radioTable)
+    {
+        switch ($radioTable->getSorting()) {
+            case RadioTable::SORTING_NAME:
+                return $this->findBy(
+                    ['radioTable' => $radioTable],
+                    [RadioTable::SORTING_NAME => 'ASC', RadioTable::SORTING_FREQUENCY => 'ASC']
+                );
+
+            case RadioTable::SORTING_FREQUENCY:
+                return $this->findBy(
+                    ['radioTable' => $radioTable],
+                    [RadioTable::SORTING_FREQUENCY => 'ASC']
+                );
+
+            case RadioTable::SORTING_PRIVATE_NUMBER:
+                return $this->createQueryBuilder('radioStation')
+                    ->addSelect(
+                        // It's needed to move radiostations without private number to the end of the radiotable.
+                        'CASE WHEN radioStation.privateNumber = 0 THEN 1 ELSE 0 END AS HIDDEN privateNumberEmpty'
+                    )
+                    ->andWhere('radioStation.radioTable = :radioTable')
+                    ->setParameter('radioTable', $radioTable)
+                    ->addOrderBy('privateNumberEmpty', 'ASC')
+                    ->addOrderBy('radioStation.privateNumber', 'ASC')
+                    ->addOrderBy('radioStation.frequency', 'ASC')
+                    ->getQuery()
+                    ->getResult()
+                ;
+                break;
+        }
+
     }
 
 //    /**
