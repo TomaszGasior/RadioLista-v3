b15506628e15dffea0708440c2d0289aced7a1ff
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2018-09-02 23:03:13 +0200

    Port UserController actions to RadioTablesListRenderer.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Renderer\RadioTablesListRenderer;
 use App\Repository\RadioTableRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,23 +13,34 @@ class UserController extends AbstractController
     /**
      * @Route("/profil/{name}", name="user.public_profile")
      */
-    public function publicProfile(User $user)
+    public function publicProfile(User $user, RadioTablesListRenderer $radioTablesListRenderer)
     {
         if (!$user->getPublicProfile()) {
             throw $this->createNotFoundException();
         }
 
+        $radioTablesList = $radioTablesListRenderer->render(
+            $user->getRadioTables(),
+            null
+        );
+
         return $this->render('user/public-profile.html.twig', [
             'user' => $user,
+            'radioTablesList' => $radioTablesList,
         ]);
     }
 
     /**
      * @Route("/moje-wykazy", name="user.my_radiotables")
      */
-    public function myRadioTables(RadioTableRepository $radioTableRepository)
+    public function myRadioTables(RadioTableRepository $radioTableRepository,
+                                  RadioTablesListRenderer $radioTablesListRenderer)
     {
-        $radioTablesList = $radioTableRepository->findOwnedByUser($this->getUser());
+        $radioTables = $radioTableRepository->findOwnedByUser($this->getUser());
+        $radioTablesList = $radioTablesListRenderer->render(
+            $radioTables,
+            RadioTablesListRenderer::OPTION_SHOW_VISIBILITY | RadioTablesListRenderer::OPTION_SHOW_ACTIONS
+        );
 
         return $this->render('user/my-radiotables.html.twig', [
             'radioTablesList' => $radioTablesList,
