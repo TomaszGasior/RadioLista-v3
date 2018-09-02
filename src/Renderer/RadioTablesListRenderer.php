57c2a665e305bd0b83bcbbbb3c6e1e594bbf4d2a
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2018-09-02 23:00:10 +0200

    Introduce RadioTablesListRenderer class.
    Port proper template to renderer.

---
+++ src/Renderer/RadioTablesListRenderer.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Renderer;
+
+use App\Entity\RadioTable;
+
+class RadioTablesListRenderer
+{
+    public const OPTION_USE_CACHE       = 0b00001;
+    public const OPTION_SHOW_OWNER      = 0b00010;
+    public const OPTION_SHOW_VISIBILITY = 0b00100;
+    public const OPTION_SHOW_ACTIONS    = 0b01000;
+
+    private $twig;
+
+    public function __construct(\Twig_Environment $twig)
+    {
+        $this->twig = $twig;
+    }
+
+    public function render(iterable $radioTables, ?int $options = self::OPTION_SHOW_OWNER)
+    {
+        if (empty($radioTables)) {
+            return '';
+        }
+
+        return $this->process($radioTables, $options);
+    }
+
+    private function process(iterable $radioTables, ?int $options)
+    {
+        return $this->twig->render('renderer/radiotables-list.html.twig', [
+            'radioTables' => $radioTables,
+
+            'showOwner'      => ($options & self::OPTION_SHOW_OWNER),
+            'showActions'    => ($options & self::OPTION_SHOW_VISIBILITY),
+            'showVisibility' => ($options & self::OPTION_SHOW_ACTIONS),
+        ]);
+    }
+}
