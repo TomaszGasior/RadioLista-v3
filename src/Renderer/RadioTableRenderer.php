67f46f5ee724c3d02e0acef88351d352f0b58f94
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2018-09-07 02:07:52 +0200

    RadioTableRenderer: implement RDS column.
    Needs some work — current implementation allows XSS attack on data-* HTML attrs.

--- src/Renderer/RadioTableRenderer.php
+++ src/Renderer/RadioTableRenderer.php
@@ -42,11 +42,34 @@ class RadioTableRenderer
 
         $radioStations = $this->radioStationRepository->findForRadioTable($radioTable);
 
+        $this->twig->addFilter(new \Twig_Filter('format_rds_frames', [$this, 'formatRDSFrames']));
+
         return $this->twig->render('renderer/radiotable.html.twig', [
             'radiotable'      => $radioTable,
             'radiostations'   => $radioStations,
             'visible_columns' => $visibleColumns,
             'show_edit_links' => ($options & self::OPTION_SHOW_EDIT_LINKS),
         ]);
+    }
+
+    public function formatRDSFrames(array $frames): array
+    {
+        foreach($frames as &$frame) {
+            $frame      = str_replace('_', ' ', $frame);
+            $emptyChars = 8 - mb_strlen($frame);
+
+            if ('' == trim($frame)) {
+                continue;
+            }
+
+            if ($emptyChars > 0) {
+                $frame = str_repeat(' ', floor($emptyChars/2)) . $frame . str_repeat(' ', ceil($emptyChars/2));
+            }
+            elseif ($emptyChars < 0) {
+                $frame = htmlspecialchars(substr($frame, 0, 8));
+            }
+        }
+
+        return $frames;
     }
 }
