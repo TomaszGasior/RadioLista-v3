1dc29f6383c2c600496eb967ed23d27198af96ca
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2018-09-09 04:16:50 +0200

    RadioTableSettingsType: add appearance settings.

--- src/Form/RadioTableSettingsType.php
+++ src/Form/RadioTableSettingsType.php
@@ -5,7 +5,11 @@ namespace App\Form;
 use App\Entity\RadioTable;
 use App\Form\Extension\LabeledCollectionType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
@@ -35,6 +39,42 @@ class RadioTableSettingsType extends RadioTableCreateType
                     'rds'           => 'RDS',
                     'comment'       => 'Komentarz',
                 ],
+            ])
+            ->add('appearanceTheme', ChoiceType::class, [
+                'property_path' => 'appearance[th]',
+
+                'expanded' => true,
+                'choices'  => [
+                    'Bez motywu'    => '',
+                    'Górski las'    => 'bieszczady',
+                    'Drewno'        => 'wood',
+                    'Tęcza i niebo' => 'rainbow',
+                    'Widok nocą'    => 'night',
+                ],
+            ])
+            ->add('appearanceBackgroundColor', ColorType::class, [
+                'property_path' => 'appearance[bg]',
+
+                'label'    => 'Kolor tła',
+                'required' => false,
+            ])
+            ->add('appearanceColor', ColorType::class, [
+                'property_path' => 'appearance[fg]',
+
+                'label'    => 'Kolor tekstu',
+                'required' => false,
+            ])
+            ->add('appearanceBackgroundImage', UrlType::class, [
+                'property_path' => 'appearance[img]',
+
+                'label'    => 'Adres URL obrazu tła',
+                'required' => false,
+            ])
+            ->add('appearanceFullWidth', CheckboxType::class, [
+                'property_path' => 'appearance[full]',
+
+                'label'    => 'Zawsze wykorzystuj pełną szerokość ekranu',
+                'required' => false,
             ])
         ;
     }
