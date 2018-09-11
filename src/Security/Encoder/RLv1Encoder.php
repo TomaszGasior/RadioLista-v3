7597657772b51ef188f8a5a386873a4fda2c82da
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2018-09-11 20:28:25 +0200

    Minor change.

--- src/Security/Encoder/RLv1Encoder.php
+++ src/Security/Encoder/RLv1Encoder.php
@@ -6,7 +6,8 @@ use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
 
 class RLv1Encoder extends MessageDigestPasswordEncoder
 {
-    public function __construct(string $algorithm = 'sha512', bool $encodeHashAsBase64 = true, int $iterations = 5000)
+    public function __construct(string $algorithm = 'sha512', bool $encodeHashAsBase64 = true,
+                                int $iterations = 5000)
     {
         parent::__construct('sha1', false, 1);
     }
