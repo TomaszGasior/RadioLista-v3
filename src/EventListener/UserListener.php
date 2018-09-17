1ace35a25a19138f6621cade8caf453af72beddb
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2018-09-17 02:57:07 +0200

    Update UserListener.
    - Port RLv2 database trigger.
    - Automatically encode plain password.

--- src/EventListener/UserListener.php
+++ src/EventListener/UserListener.php
@@ -2,6 +2,42 @@
 
 namespace App\EventListener;
 
+use App\Entity\User;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Doctrine\ORM\Mapping\PreFlush;
+use Doctrine\ORM\Mapping\PreUpdate;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
 class UserListener
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @PreUpdate
+     */
+    public function refreshLastActivityDate(User $user, PreUpdateEventArgs $args): void
+    {
+        if ($args->hasChangedField('aboutMe') || $args->hasChangedField('publicProfile')) {
+            $user->refreshLastActivityDate();
+        }
+    }
+
+    /**
+     * @PreFlush
+     */
+    public function encodePassword(User $user): void
+    {
+        if (null === $user->getPlainPassword()) {
+            return;
+        }
+
+        $user->setPassword(
+            $this->passwordEncoder->encodePassword($user, $user->getPlainPassword())
+        );
+    }
 }
