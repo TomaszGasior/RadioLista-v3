b3b40e176fabddc4e557ec729cae3ca024cfe2dc
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2018-10-21 21:35:00 +0200

    Implement UserController::register() action.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,11 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserRegisterType;
 use App\Renderer\RadioTablesListRenderer;
 use App\Repository\RadioTableRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -61,8 +64,28 @@ class UserController extends AbstractController
      * @Route("/rejestracja", name="user.register")
      * @Security("not is_granted('IS_AUTHENTICATED_REMEMBERED')", statusCode=400)
      */
-    public function register(): Response
+    public function register(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('user/register.html.twig');
+        $user = new User;
+
+        $registerForm = $this->createForm(UserRegisterType::class, $user);
+        $registerForm->handleRequest($request);
+
+        if ($registerForm->isSubmitted()) {
+            if ($registerForm->isValid()) {
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                $this->addFlash('notice', 'Twoje konto zostało utworzone.');
+                return $this->redirectToRoute('security.login');
+            }
+            else {
+                $this->addFlash('error', $registerForm->getErrors(true));
+            }
+        }
+
+        return $this->render('user/register.html.twig', [
+            'register_form' => $registerForm->createView(),
+        ]);
     }
 }
