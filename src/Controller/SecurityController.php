ce15d3bfc20367e7ea14b859bc91a92fdfe987fe
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2018-10-24 00:09:34 +0200

    SecurityController::login(): translate error message.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class SecurityController extends AbstractController
 {
@@ -15,7 +16,8 @@ class SecurityController extends AbstractController
      * @Route("/logowanie", name="security.login")
      * @Security("not is_granted('IS_AUTHENTICATED_REMEMBERED')", statusCode=400)
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils,
+                          TranslatorInterface $translator): Response
     {
         $form = $this->createForm(SecurityLoginType::class, [
             'username' => $authenticationUtils->getLastUsername(),
@@ -23,7 +25,10 @@ class SecurityController extends AbstractController
 
         $error = $authenticationUtils->getLastAuthenticationError();
         if ($error) {
-            $this->addFlash('error', $error->getMessageKey());
+            $this->addFlash(
+                'error',
+                $translator->trans($error->getMessageKey(), [], 'security')
+            );
         }
 
         return $this->render('security/login.html.twig', [
