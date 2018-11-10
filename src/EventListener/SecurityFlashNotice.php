6c35ea16c62f0e7d159a5ebb843ea43cf1bc0c7e
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2018-11-10 15:24:52 +0100

    Do not show log-in flash notice when user is remembered.

--- src/EventListener/SecurityFlashNotice.php
+++ src/EventListener/SecurityFlashNotice.php
@@ -5,18 +5,26 @@ namespace App\EventListener;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Core\Security;
 
 class SecurityFlashNotice
 {
     private $requestStack;
+    private $security;
 
-    public function __construct(RequestStack $requestStack)
+    public function __construct(RequestStack $requestStack, Security $security)
     {
         $this->requestStack = $requestStack;
+        $this->security = $security;
     }
 
     public function onSecurityInteractiveLogin()
     {
+        // Do not show login message when user is logged in by "remember me" feature.
+        if (!$this->security->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return false;
+        }
+
         $session = $this->requestStack->getCurrentRequest()->getSession();
 
         $session->getFlashBag()->add('notice', 'Zalogowano się pomyślnie.');
