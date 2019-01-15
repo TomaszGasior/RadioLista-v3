af3d225bddf787ba11478815604a1b3d98da200b
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-01-15 22:17:16 +0100

    Install Doctrine fixtures and Faker

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
