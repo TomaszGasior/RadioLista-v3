25a36296f663c05cc5e7028e63615f1f9a7f37f3
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-01-28 15:40:30 +0100

    Port dark errors from RLv2
    - Disable Twig routes in dev environment. They don't work with current exception controller.

---
+++ src/Controller/DarkErrorController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\RadioTable;
+use Doctrine\DBAL\Exception\ConnectionException;
+use Symfony\Bundle\TwigBundle\Controller\ExceptionController;
+use Symfony\Component\Debug\Exception\FlattenException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
+
+class DarkErrorController extends ExceptionController
+{
+    public function show(Request $request, FlattenException $exception, DebugLoggerInterface $logger = null): Response
+    {
+        if ($this->debug) {
+            return parent::showAction($request, $exception, $logger);
+        }
+
+        $message = null;
+
+        if (is_a($exception->getClass(), ConnectionException::class, true)) {
+            $message = 'DatabaseConnection';
+        }
+        elseif (is_a($exception->getClass(), HttpException::class, true) and 404 === $exception->getStatusCode()) {
+            $message = 'NotFound';
+
+            if (false !== strpos($exception->getMessage(), RadioTable::class)
+                || false !== strpos($exception->getMessage(), 'RADIOTABLE_SHOW')) {
+                $message = 'RadioTableNotFound';
+            }
+        }
+
+        return new Response(
+            $this->twig->render('dark-error.html.twig', ['message' => $message])
+        );
+    }
+}
