0715db88b8396cfc783eee0664a33f76f730d049
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-03-02 17:28:26 +0100

    Add test for RehashRLv1UserPassword

---
+++ tests/EventSubscriber/RehashRLv1UserPasswordTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Tests\EventSubscriber;
+
+use App\Entity\User;
+use App\EventSubscriber\RehashRLv1UserPassword;
+use Doctrine\ORM\EntityManagerInterface;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+
+class RehashRLv1UserPasswordTest extends TestCase
+{
+    private const PLAIN_PASSWORD = 'Passw0rd!';
+
+    public function testRehashPasswordFromOldService(): void
+    {
+        $user = $this->createMock(User::class);
+        $user
+            ->method('getEncoderName')
+            ->willReturn('rl_v1')
+        ;
+        $user
+            ->expects($this->once())
+            ->method('setPlainPassword')
+            ->with($this->equalTo(self::PLAIN_PASSWORD))
+        ;
+
+        $entityManager = $this->createMock(EntityManagerInterface::class);
+        $entityManager
+            ->expects($this->once())
+            ->method('flush')
+            ->with($this->equalTo($user))
+        ;
+
+        $token = $this->createMock(TokenInterface::class);
+        $token
+            ->method('getUser')
+            ->willReturn($user)
+        ;
+        $token
+            ->method('getCredentials')
+            ->willReturn(self::PLAIN_PASSWORD)
+        ;
+        $token
+            ->expects($this->once())
+            ->method('eraseCredentials')
+        ;
+
+        $request = $this->createMock(Request::class);
+        $securityEvent = new InteractiveLoginEvent($request, $token);
+
+        $subscriber = new RehashRLv1UserPassword($entityManager);
+        $subscriber->onSecurityInteractiveLogin($securityEvent);
+    }
+
+    public function testDoNotRehashPassword(): void
+    {
+        $user = $this->createMock(User::class);
+        $user
+            ->method('getEncoderName')
+            ->willReturn('rl_v2')
+        ;
+        $user
+            ->expects($this->never())
+            ->method('setPlainPassword')
+        ;
+
+        $entityManager = $this->createMock(EntityManagerInterface::class);
+        $entityManager
+            ->expects($this->never())
+            ->method('flush')
+        ;
+
+        $token = $this->createMock(TokenInterface::class);
+        $token
+            ->method('getUser')
+            ->willReturn($user)
+        ;
+        $token
+            ->method('getCredentials')
+            ->willReturn(self::PLAIN_PASSWORD)
+        ;
+        $token
+            ->expects($this->once())
+            ->method('eraseCredentials')
+        ;
+
+        $request = $this->createMock(Request::class);
+        $securityEvent = new InteractiveLoginEvent($request, $token);
+
+        $subscriber = new RehashRLv1UserPassword($entityManager);
+        $subscriber->onSecurityInteractiveLogin($securityEvent);
+    }
+}
