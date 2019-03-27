cc8f0f0079ab50935fa632fddb34034c7a142e5d
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-03-27 19:05:18 +0100

    Reorganize functional tests

---
+++ tests/Functional/BasicApplicationTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class BasicApplicationTest extends WebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testApplicationWorks(string $url): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', $url);
+
+        $response = $client->getResponse();
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertContains('<!doctype html>', $response->getContent());
+        $this->assertContains('<html', $response->getContent());
+        $this->assertContains('<body', $response->getContent());
+        $this->assertContains('</body>', $response->getContent());
+        $this->assertContains('</html>', $response->getContent());
+    }
+
+    public function urlProvider(): array
+    {
+        self::bootKernel();
+        $userRepository = self::$container->get('App\Repository\UserRepository');
+        $radioTableRepository = self::$container->get('App\Repository\RadioTableRepository');
+
+        $radioTables = $radioTableRepository->findPublicOrderedByLastUpdateTime(10);
+        $users = $userRepository->findByPublicProfile(true, null, 10);
+
+        $basicURLs = [
+            [''],
+            ['/strona-glowna'],
+            ['/o-stronie'],
+            ['/regulamin'],
+            ['/kontakt'],
+            ['/wszystkie-wykazy'],
+            ['/wszystkie-wykazy/2'],
+            ['/wszystkie-wykazy/3'],
+            ['/logowanie'],
+            ['/rejestracja'],
+        ];
+
+        $radioTableURLs = [];
+        $usersURLs = [];
+
+        foreach ($radioTables as $radioTable) {
+            $radioTableURLs[] = ['/wykaz/' . $radioTable->getId()];
+        }
+        foreach ($users as $user) {
+            $userURLs[] = ['/profil/' . $user->getName()];
+        }
+
+        return array_merge($basicURLs, $radioTableURLs, $userURLs);
+    }
+}
--- tests/Functional/SecurityPermissionTest.php
+++ tests/Functional/SecurityPermissionTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Tests\Controller;
+namespace App\Tests\Functional;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
