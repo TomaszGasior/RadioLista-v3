f9da6b63d2c0d3fd85c72860ca0e5f57083fe127
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-03-29 22:28:48 +0100

    Update BasicApplicationTest

--- tests/Functional/BasicApplicationTest.php
+++ tests/Functional/BasicApplicationTest.php
@@ -7,9 +7,9 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class BasicApplicationTest extends WebTestCase
 {
     /**
-     * @dataProvider urlProvider
+     * @dataProvider publicUrlsProvider
      */
-    public function testApplicationSeemsToWork(string $url): void
+    public function testPublicPagesSeemToWork(string $url): void
     {
         $client = static::createClient();
         $client->request('GET', $url);
@@ -17,13 +17,10 @@ class BasicApplicationTest extends WebTestCase
         $response = $client->getResponse();
         $this->assertSame(200, $response->getStatusCode());
         $this->assertContains('<!doctype html>', $response->getContent());
-        $this->assertContains('<html', $response->getContent());
-        $this->assertContains('<body', $response->getContent());
-        $this->assertContains('</body>', $response->getContent());
         $this->assertContains('</html>', $response->getContent());
     }
 
-    public function urlProvider(): iterable
+    public function publicUrlsProvider(): iterable
     {
         self::bootKernel();
         $userRepository = self::$container->get('App\Repository\UserRepository');
@@ -49,5 +46,43 @@ class BasicApplicationTest extends WebTestCase
         foreach ($users as $user) {
             yield ['/profil/' . $user->getName()];
         }
+    }
+
+    /**
+     * @dataProvider authenticatedUrlsProvider
+     */
+    public function testAuthenticatedPagesSeemToWork(string $url): void
+    {
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'radiolista',
+            'PHP_AUTH_PW' => 'radiolista',
+        ]);
+        $client->request('GET', $url);
+
+        $response = $client->getResponse();
+        $this->assertSame(200, $response->getStatusCode());
+        $this->assertContains('<!doctype html>', $response->getContent());
+        $this->assertContains('</html>', $response->getContent());
+    }
+
+    public function authenticatedUrlsProvider(): array
+    {
+        return [
+            ['/utworz-wykaz'],
+            ['/moje-wykazy'],
+            ['/ustawienia-konta'],
+        ];
+    }
+
+    public function testSitemapSeemsToWork(): void
+    {
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'radiolista',
+            'PHP_AUTH_PW' => 'radiolista',
+        ]);
+        $client->request('GET', '/sitemap.xml');
+
+        $response = $client->getResponse();
+        $this->assertSame(200, $response->getStatusCode());
     }
 }
