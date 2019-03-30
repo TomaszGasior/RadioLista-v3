a8a117b5aee184a0190e834ef424fdfc2c200740
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-03-30 19:54:34 +0100

    Use CKEditor in forms

--- src/Form/RadioTableCreateType.php
+++ src/Form/RadioTableCreateType.php
@@ -3,9 +3,9 @@
 namespace App\Form;
 
 use App\Entity\RadioTable;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -24,7 +24,7 @@ class RadioTableCreateType extends AbstractType
                     'MHz' => 0,
                 ]
             ])
-            ->add('description', TextareaType::class, [
+            ->add('description', CKEditorType::class, [
                 'label' => 'Opis wykazu',
                 'required' => false,
                 'sanitize_html' => true,
--- src/Form/UserSettingsType.php
+++ src/Form/UserSettingsType.php
@@ -3,11 +3,11 @@
 namespace App\Form;
 
 use App\Entity\User;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,7 +19,7 @@ class UserSettingsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('aboutMe', TextareaType::class, [
+            ->add('aboutMe', CKEditorType::class, [
                 'label' => 'Kilka słów o mnie',
                 'required' => false,
                 'sanitize_html' => true,
