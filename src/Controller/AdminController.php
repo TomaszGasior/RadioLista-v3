22cdede762ddc5610d4835b32894ea42d7543201
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-04-09 13:15:46 +0200

    Security for AdminController

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,11 +4,15 @@ namespace App\Controller;
 
 use App\Repository\RadioTableRepository;
 use App\Repository\UserRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @IsGranted("ROLE_ADMIN", statusCode=404)
+ */
 class AdminController extends AbstractController
 {
     /**
