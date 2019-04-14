bc56776ef596c7afa5588d5291ba3050e2fc825b
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-04-14 04:03:05 +0200

    Fix custom twig compiler
    Don't minify <pre> tags.

--- src/Twig/Compiler.php
+++ src/Twig/Compiler.php
@@ -8,6 +8,8 @@ use Twig\Node\TextNode;
 
 class Compiler extends BaseCompiler
 {
+    private $ignoredTemplates = [];
+
     public function compile(Node $node, $indentation = 0): self
     {
         $this->compressTextNodes($node);
@@ -24,25 +26,40 @@ class Compiler extends BaseCompiler
 
     private function compressTextNodes(Node $node): void
     {
-        // Don't try to minify CSV files and bundles templates.
-        if (false !== strpos($node->getTemplateName(), '.csv.twig') ||
-            false !== strpos($node->getTemplateName(), '@')) {
+        if (in_array($node->getTemplateName(), $this->ignoredTemplates)) {
             return;
+        }
+
+        // Don't try to minify CSV files.
+        if (false !== strpos($node->getTemplateName(), '.csv.twig')) {
+            return;
+        }
+
+        foreach ($node as $childNode) {
+            $this->{__FUNCTION__}($childNode);
         }
 
         if ($node instanceof TextNode) {
             $data = $node->getAttribute('data');
 
-            // Yep, this "minification" sucks. It breaks <pre>/<code> contents
-            // and inline JavaScript without semicolons.
-            // But it seems to be fine for this application purposes.
-            $data = preg_replace('/\s+/', ' ', $data);
+            // Don't minify pages with <pre> tags.
+            if (false !== mb_strpos($data, '<pre')) {
+                $this->ignoredTemplates[] = $node->getTemplateName();
+                return;
+            }
+
+            // Only remove empty lines and repeated spaces in templates of external bundles.
+            if ('@' === $node->getTemplateName()[0]) {
+                $data = preg_replace(['/^\n+/m', '/ +/'], ['', ' '], $data);
+            }
+            // Replace all whitespace character sequences with just one space. This kind
+            // of "minification" might break inline JS scripts but it seems to be fine
+            // for this application. Do it only for application's own templates.
+            else {
+                $data = preg_replace('/\s+/', ' ', str_replace("\n", '', $data));
+            }
 
             $node->setAttribute('data', $data);
-        }
-
-        foreach ($node as $childNode) {
-            $this->{__FUNCTION__}($childNode);
         }
     }
 }
