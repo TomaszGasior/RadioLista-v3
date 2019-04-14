ac36c7d46aa91c290ee42d13027787de23311cf9
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-04-15 00:40:10 +0200

    Add AdminControllerTest

---
+++ tests/Controller/AdminControllerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AdminControllerTest extends WebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testAdminPanelSeemsToWork($url): void
+    {
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'radiolista',
+            'PHP_AUTH_PW' => 'radiolista',
+        ]);
+
+        $client->request('GET', $url);
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testAdminPanelInaccessibleForNonAdmin($url): void
+    {
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'test_user',
+            'PHP_AUTH_PW' => 'test_user',
+        ]);
+
+        $client->request('GET', $url);
+        $this->assertSame(404, $client->getResponse()->getStatusCode());
+
+        $client = static::createClient();
+
+        $client->request('GET', $url);
+        $this->assertSame(404, $client->getResponse()->getStatusCode());
+    }
+
+    public function urlProvider(): array
+    {
+        return [
+            ['/admin/dziennik'],
+            ['/admin/wykazy'],
+            ['/admin/uzytkownicy'],
+        ];
+    }
+}
