2d2fbe562a363ea2d5d04954df51e96ae8df2f0f
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-04-17 14:57:45 +0200

    Update DarkErrorController

--- src/Controller/DarkErrorController.php
+++ src/Controller/DarkErrorController.php
@@ -13,26 +13,31 @@ use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
 
 class DarkErrorController extends ExceptionController
 {
-    public function show(Request $request, FlattenException $exception, DebugLoggerInterface $logger = null): Response
+    public function show(Request $request, FlattenException $exception,
+                         DebugLoggerInterface $logger = null): Response
     {
         if ($this->debug) {
             return parent::showAction($request, $exception, $logger);
         }
 
+        $class = $exception->getClass();
         $message = null;
 
-        if (is_a($exception->getClass(), ConnectionException::class, true)) {
+        if (is_a($class, ConnectionException::class, true)) {
             $message = 'DatabaseConnection';
         }
-        elseif (is_a($exception->getClass(), HttpException::class, true)) {
-            if (403 === $exception->getStatusCode() || 400 === $exception->getStatusCode()) {
+        elseif (is_a($class, HttpException::class, true)) {
+            $statusCode = $exception->getStatusCode();
+
+            if (403 === $statusCode || 400 === $statusCode) {
                 $message = 'UnexpectedRequest';
             }
-            elseif (404 === $exception->getStatusCode()) {
+            elseif (404 === $statusCode) {
                 $message = 'NotFound';
 
-                if (false !== strpos($exception->getMessage(), RadioTable::class)
-                    || false !== strpos($exception->getMessage(), 'RADIOTABLE_SHOW')) {
+                $text = $exception->getMessage();
+                if (false !== strpos($text, RadioTable::class) ||
+                    false !== strpos($text, 'RADIOTABLE_SHOW')) {
                     $message = 'RadioTableNotFound';
                 }
             }
