7a5142365d61ab84b7ece56420c48a9ede2c6c41
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-07-15 19:42:31 +0200

    DecimalUnitType: use NumberType as parent with html5 option
    Fixes #40

--- src/Form/Type/DecimalUnitType.php
+++ src/Form/Type/DecimalUnitType.php
@@ -3,7 +3,7 @@
 namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,10 +12,7 @@ class DecimalUnitType extends AbstractType
 {
     public function getParent(): string
     {
-        // Do not use NumberType as parent. NumberType defines additional view transformer that formats
-        // value according to (Polish) locale settings. This causes comma to be used as decimal point
-        // instead dot. `<input type="number" value="...">` tag requires standard formatting with dot.
-        return TextType::class;
+        return NumberType::class;
     }
 
     public function finishView(FormView $view, FormInterface $form, array $options): void
@@ -28,8 +25,10 @@ class DecimalUnitType extends AbstractType
     {
         $resolver
             ->setDefaults(['unit_label' => ''])
+            ->setDefaults(['html5' => true])
             ->setRequired(['step'])
             ->setAllowedTypes('step', 'float')
+            ->setAllowedValues('html5', true)
         ;
     }
 }
