54120567517b8e65c912085e6bcfc484a1ef0697
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-08-24 22:58:32 +0200

    Docker: define own entrypoint script
    Entrypoint script creates database and prepares fixtures automatically
    on container's first boot.

---
+++ .docker/php-fpm/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+# Fix improper `var` permission caused by docker volume.
+chmod 777 var
+
+# Wait until database will be ready.
+until nc -z db 3306; do sleep 1; done; sleep 1
+
+# Create database with example data if doesn't exist.
+if ! bin/console doctrine:query:sql "show tables" -q 2> /dev/null; then
+    bin/console doctrine:database:create
+    bin/console doctrine:schema:create
+    bin/console doctrine:fixtures:load -n
+fi
+
+docker-php-entrypoint "$@"
