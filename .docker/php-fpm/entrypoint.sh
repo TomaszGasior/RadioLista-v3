538e30f84344a78ed0801bb44a9c415dfbef8c2d
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-08-29 01:02:30 +0200

    Docker: run `composer install` when needed
    Since there is `composer` binary inside app container, this way
    running the application will be easier.

--- .docker/php-fpm/entrypoint.sh
+++ .docker/php-fpm/entrypoint.sh
@@ -4,6 +4,11 @@ set -e
 # Fix improper `var` permission caused by docker volume.
 chmod 777 var
 
+# Make it easier to run the application first time. :)
+if [[ ! -d vendor ]]; then
+    su-exec $(stat -c '%u:%g' .) composer install -n --no-cache
+fi
+
 # Wait until database will be ready.
 until nc -z db 3306; do sleep 1; done; sleep 1
 
