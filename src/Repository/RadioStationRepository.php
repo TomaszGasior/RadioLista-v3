77892a65861d790544d6c0b46342722885739a38
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2020-01-29 03:05:13 +0100

    Fix deprecation in entity repositories

--- src/Repository/RadioStationRepository.php
+++ src/Repository/RadioStationRepository.php
@@ -6,7 +6,7 @@ use App\Entity\RadioStation;
 use App\Entity\RadioTable;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\QueryBuilder;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method RadioStation|null find($id, $lockMode = null, $lockVersion = null)
@@ -16,7 +16,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class RadioStationRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, RadioStation::class);
     }
--- src/Repository/RadioTableRepository.php
+++ src/Repository/RadioTableRepository.php
@@ -5,7 +5,7 @@ namespace App\Repository;
 use App\Entity\RadioTable;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method RadioTable|null find($id, $lockMode = null, $lockVersion = null)
@@ -15,7 +15,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class RadioTableRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, RadioTable::class);
     }
