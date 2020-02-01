bc78de5043eb4ebdd42836e9b54a17df9639a176
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2020-02-01 02:06:22 +0100

    Introduce Deployer configuration
    Fixes #58

---
+++ deploy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/symfony4.php';
+
+set('repository', 'git@github.com:TomaszGasior/RadioLista-v3.git');
+set('branch', function(){ return runLocally('git describe master --abbrev=0'); });
+
+set('env', ['APP_ENV' => 'prod']);
+set('shared_files', ['.env.local.php']);
+
+inventory('deploy.yaml');
+
+desc('Clear PHP opcache');
+task('deploy:clear_opcache', 'cachetool opcache:reset --web --web-path {{public_dir}} --web-url http://{{domain}}');
+after('deploy:symlink', 'deploy:clear_opcache');
+
+desc('Enable maintenance mode');
+task('maintenance:enable', 'touch maintenance.lock');
+
+desc('Disable maintenance mode');
+task('maintenance:disable', 'rm -f maintenance.lock');
+
+desc('Upload generated .env.local.php file');
+task('deploy:env_local_php', function(){
+    if (test('[ -f {{deploy_path}}/shared/.env.local.php ]')) {
+        throw new \Exception('File .env.local.php already exists.');
+    }
+    runLocally('composer symfony:dump-env prod');
+    upload('.env.local.php', '{{deploy_path}}/shared/.env.local.php');
+    runLocally('rm .env.local.php');
+});
+before('deploy:env_local_php', 'deploy:prepare');
