234a16c4dd4ad4c5df910d4f53ca9fbad3b96d8c
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2020-05-13 22:04:46 +0200

    DarkErrorController: set preferred locale for real 404s

--- src/Controller/DarkErrorController.php
+++ src/Controller/DarkErrorController.php
@@ -6,25 +6,40 @@ use App\Entity\RadioTable;
 use Doctrine\DBAL\Exception\ConnectionException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Controller\ErrorController;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Translation\Translator;
 
 class DarkErrorController extends AbstractController
 {
     private $errorController;
     private $debug;
+    private $translator;
+    private $locales;
+    private $requestStack;
 
-    public function __construct(ErrorController $errorController, bool $debug)
+    public function __construct(ErrorController $errorController, Translator $translator,
+                                RequestStack $requestStack, bool $debug, array $locales)
     {
         $this->errorController = $errorController;
+        $this->translator = $translator;
         $this->debug = $debug;
+        $this->locales = $locales;
+        $this->requestStack = $requestStack;
     }
 
     public function showError(\Throwable $exception, Request $request): Response
     {
         if ($request->attributes->get('showException', $this->debug)) {
             return ($this->errorController)($exception);
+        }
+
+        // Site language is defined by locale assigned to route. Set preferred
+        // language for better user experience when route is unknown (real 404).
+        if (!$this->requestStack->getMasterRequest()->attributes->get('_route')) {
+            $this->translator->setLocale($request->getPreferredLanguage($this->locales));
         }
 
         $message = null;
