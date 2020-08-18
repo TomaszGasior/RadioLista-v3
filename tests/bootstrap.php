6e1714c3f0d1e6824d551ad3f3d8f3ae187ebdba
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2020-08-18 11:06:31 +0200

    Get rid of bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,8 +3,15 @@
 use App\Kernel;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Dotenv\Dotenv;
 
-require __DIR__ . '/bootstrap.php';
+require dirname(__DIR__).'/vendor/autoload.php';
+
+if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
+    require dirname(__DIR__).'/config/bootstrap.php';
+} elseif (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}
 
 (function(){
     $kernel = new Kernel('test', true);
