c317c874bfd648a669f6f39aed437924a9f8f3a6
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2021-11-30 21:00:34 +0100

    RadioStationEditExtension: refactor & fix RDS PI handling
    Fixes #169

--- src/Form/Extension/RadioStationEditExtension.php
+++ src/Form/Extension/RadioStationEditExtension.php
@@ -9,26 +9,52 @@ use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 
+/**
+ * This extension is designed for RadioStationEditType.
+ *
+ * It adds extra information into form view to make it possible
+ * to render form fields for radio table's disabled columns different way.
+ */
 class RadioStationEditExtension extends AbstractTypeExtension
 {
+    /**
+     * These form fields aren't radio table columns
+     * and should never be rendered as disabled.
+     */
+    private const PROPERTY_PATH_NON_COLUMNS = [
+        'marker',
+    ];
+
+    /**
+     * In most cases property path is the same as column name
+     * but these form fields contain values from embeddable objects
+     * and require special handling of property path.
+     */
+    private const PROPERTY_PATH_TO_COLUMN = [
+        'locality.city' => RadioTable::COLUMN_LOCALITY,
+        'locality.type' => RadioTable::COLUMN_LOCALITY,
+        'rds.pi' => RadioTable::COLUMN_RDS_PI,
+        'rds.ps' => RadioTable::COLUMN_RDS,
+        'rds.pty' => RadioTable::COLUMN_RDS,
+        'rds.rt' => RadioTable::COLUMN_RDS,
+    ];
+
     public function finishView(FormView $view, FormInterface $form, array $options): void
     {
+        /** @var RadioStation */
         $radioStation = $form->getData();
-
-        // It's probably impossible but let's check it, just in case.
-        if (!($radioStation instanceof RadioStation)) {
-            return;
-        }
 
         $visibleColumns = $radioStation->getRadioTable()->getColumns();
         $formChildrenToHide = [];
-        $excludedChildren = ['marker'];
 
+        /** @var FormInterface[] $form */
         foreach ($form as $childrenName => $children) {
-            // Get only first element of property path. Some RadioStation properties
-            // (like locality or rds) use additional elements to access array key.
-            // To get proper name of RadioTable column it's needed to ignore them.
-            $columnName = $children->getPropertyPath()->getElement(0);
+            $propertyPath = (string) $children->getPropertyPath();
+            $columnName = self::PROPERTY_PATH_TO_COLUMN[$propertyPath] ?? $propertyPath;
+
+            if (in_array($columnName, self::PROPERTY_PATH_NON_COLUMNS)) {
+                continue;
+            }
 
             if (false === in_array($columnName, $visibleColumns)) {
                 $formChildrenToHide[] = $childrenName;
@@ -37,8 +63,7 @@ class RadioStationEditExtension extends AbstractTypeExtension
 
         foreach ($view as $childrenName => $children) {
             $children->vars['disabled_radio_table_column'] =
-                in_array($childrenName, $formChildrenToHide) &&
-                false === in_array($childrenName, $excludedChildren);
+                in_array($childrenName, $formChildrenToHide);
         }
     }
 
