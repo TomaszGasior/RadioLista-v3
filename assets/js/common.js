67fc7e17e1cbbe4487cff3219fb3e5a534e49d33
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2022-05-07 02:57:39 +0200

    Redesign site notifications

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,11 +1,3 @@
 import '../css/common.css';
 
 document.documentElement.classList.add('JS');
-
-document.addEventListener('DOMContentLoaded', () => {
-    let notification = document.querySelector('.notification-wrapper');
-
-    if (notification) {
-        setTimeout(() => { notification.classList.add('hidden'); }, 10000);
-    }
-});
