d53ec6bd456f1fedc053ca9e877e3b80bf842f19
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2022-05-15 23:44:46 +0200

    Forms: autofocus first invalid field when validation error occurs
    Fixes #195

--- src/Form/Extension/ValidationErrorExtension.php
+++ src/Form/Extension/ValidationErrorExtension.php
@@ -7,9 +7,22 @@ use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\HttpFoundation\RequestStack;
 
-class ValidationFlashErrorExtension extends AbstractTypeExtension
+/**
+ * This extension applies user experience tweaks when validation error occurs.
+ *
+ * * `autofocus` HTML attribute is added to all invalid form fields. Only one
+ *   form field can be autofocused by browser but adding the attribute to all
+ *   of them autofocuses first one from top of the page (order of form children
+ *   in PHP does not have to match order of fields in HTML markup).
+ *
+ * * Generic notification about "invalid form below" is shown to the user.
+ *   Here it's needed to make sure it's only one to show it only once.
+ */
+class ValidationErrorExtension extends AbstractTypeExtension
 {
     private $requestStack;
 
@@ -30,6 +43,13 @@ class ValidationFlashErrorExtension extends AbstractTypeExtension
                 $session->getFlashBag()->add('error', 'common.notification.invalid_form');
             }
         });
+    }
+
+    public function finishView(FormView $view, FormInterface $form, array $options): void
+    {
+        if ($form->isSubmitted() && false === $form->isValid()) {
+            $view->vars['attr']['autofocus'] = true;
+        }
     }
 
     static public function getExtendedTypes(): array
