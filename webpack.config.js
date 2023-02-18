ac42c60b3b93ee8a27f57cb98963a40b706c19c3
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2023-02-18 01:04:16 +0100

    Revert part of "Update files from Symfony recipes"
    This commit reverts part of 6ec9eafaa3bbb2320f4db69099e91eb6ef544137
    
    New version of Webpack Encore is not compatible with shared hosting.
    https://github.com/symfony/webpack-encore/issues/1154
    https://github.com/symfony/webpack-encore/pull/1175
    
    Fixes #218

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ Encore
     .setOutputPath('public/build/')
     // public path used by the web server to access the output path
     .setPublicPath('/build')
-    // only needed for CDN's or subdirectory deploy
+    // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
     /*
@@ -63,16 +63,15 @@ Encore
 
     .copyFiles({ from: './assets/public' })
 
-    // configure Babel
     // .configureBabel((config) => {
-    //     config.plugins.push('@babel/a-babel-plugin');
+    //     config.plugins.push('@babel/plugin-proposal-class-properties');
     // })
 
-    // enables and configure @babel/preset-env polyfills
-    //.configureBabelPresetEnv((config) => {
-    //    config.useBuiltIns = 'usage';
-    //    config.corejs = '3.23';
-    //})
+    // enables @babel/preset-env polyfills
+    // .configureBabelPresetEnv((config) => {
+    //     config.useBuiltIns = 'usage';
+    //     config.corejs = 3;
+    // })
 
     // enables Sass/SCSS support
     //.enableSassLoader()
