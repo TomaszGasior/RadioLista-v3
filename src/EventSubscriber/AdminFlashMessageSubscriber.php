9ed445cad23fb27384e8589c326f52c76a5b3fa7
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2023-04-12 00:19:07 +0200

    Admin: move UI messages to translation files
    Issue #217

--- src/EventSubscriber/AdminFlashMessageSubscriber.php
+++ src/EventSubscriber/AdminFlashMessageSubscriber.php
@@ -5,6 +5,7 @@ namespace App\EventSubscriber;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Session\FlashBagAwareSessionInterface;
+use Symfony\Component\Translation\TranslatableMessage;
 
 class AdminFlashMessageSubscriber implements EventSubscriberInterface
 {
@@ -20,10 +21,14 @@ class AdminFlashMessageSubscriber implements EventSubscriberInterface
 
         $session = $request->getSession();
 
-        if ($session instanceof FlashBagAwareSessionInterface) {
-            // This triggers warning about untranslated string.
-            $session->getFlashBag()->add('notice', 'Przeglądasz prywatny zasób jako administrator.');
+        if (!$session instanceof FlashBagAwareSessionInterface) {
+            return;
         }
+
+        $session->getFlashBag()->add(
+            'error',
+            new TranslatableMessage('common.notification.showing_private_content_as_administrator', [], 'admin')
+        );
     }
 
     /**
