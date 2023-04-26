d7d9c40a60fc3a37296d54744d2cd9fd58982006
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2023-04-26 21:55:47 +0200

    Commit CKEditor into repository, stop downloading it from CKEditor servers
    CKEditor's builder does not work correctly anymore for
    our configuration since wordcount plugin has been removed
    from their servers.
    Currently generated builds of CKEditor are broken,
    trying to load non-existent plugin.
    
    Fixes #221

---
+++ assets/public/ckeditor/build-config.js
@@ -0,0 +1,77 @@
+﻿/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license/
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) https://ckeditor.com/cke4/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) https://ckeditor.com/cke4/builder/6aef766e969bd6095a4a2ee99dcc5a0c
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) https://ckeditor.com/cke4/builder/download/6aef766e969bd6095a4a2ee99dcc5a0c
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'moono',
+	preset: 'basic',
+	ignore: [
+		'.DS_Store',
+		'.bender',
+		'.editorconfig',
+		'.gitattributes',
+		'.gitignore',
+		'.idea',
+		'.jscsrc',
+		'.jshintignore',
+		'.jshintrc',
+		'.mailmap',
+		'.npm',
+		'.nvmrc',
+		'.travis.yml',
+		'bender-err.log',
+		'bender-out.log',
+		'bender.ci.js',
+		'bender.js',
+		'dev',
+		'gruntfile.js',
+		'less',
+		'node_modules',
+		'package-lock.json',
+		'package.json',
+		'tests'
+	],
+	plugins : {
+		'autolink' : 1,
+		'basicstyles' : 1,
+		'clipboard' : 1,
+		'divarea' : 1,
+		'enterkey' : 1,
+		'entities' : 1,
+		'floatingspace' : 1,
+		'indentlist' : 1,
+		'link' : 1,
+		'list' : 1,
+		'resize' : 1,
+		'toolbar' : 1,
+		'undo' : 1,
+		'wordcount' : 1,
+		'wysiwygarea' : 1
+	},
+	languages : {
+		'en' : 1,
+		'pl' : 1
+	}
+};
