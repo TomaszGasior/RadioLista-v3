dfd0c58b008954b5801fdcf4246489cad63e6704
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2023-05-02 22:12:52 +0200

    ValidationErrorExtension: add option to disable notification

--- src/Form/Extension/ValidationErrorExtension.php
+++ src/Form/Extension/ValidationErrorExtension.php
@@ -11,6 +11,7 @@ use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Session\FlashBagAwareSessionInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
  * This extension applies user experience tweaks when validation error occurs.
@@ -29,10 +30,14 @@ class ValidationErrorExtension extends AbstractTypeExtension
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $event){
+        $builder->addEventListener(FormEvents::POST_SUBMIT, function(FormEvent $event) use ($options) {
             $form = $event->getForm();
 
-            if ($form->isRoot() && false === $form->isValid()) {
+            if (!$form->isRoot() || !$options['validation_error_generic_notification']) {
+                return;
+            }
+
+            if (!$form->isValid()) {
                 $session = $this->requestStack->getCurrentRequest()->getSession();
 
                 if ($session instanceof FlashBagAwareSessionInterface) {
@@ -44,9 +49,14 @@ class ValidationErrorExtension extends AbstractTypeExtension
 
     public function finishView(FormView $view, FormInterface $form, array $options): void
     {
-        if ($form->isSubmitted() && false === $form->isValid()) {
+        if ($form->isSubmitted() && !$form->isValid()) {
             $view->vars['attr']['autofocus'] = true;
         }
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefault('validation_error_generic_notification', true);
     }
 
     static public function getExtendedTypes(): array
