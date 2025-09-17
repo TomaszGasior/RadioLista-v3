bfcd63ebdc67eadadd9d58fbf5eee057b71fb27a
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2025-09-17 18:19:19 +0200

    BasicApplicationTest: add English URLs

--- tests/Functional/BasicApplicationTest.php
+++ tests/Functional/BasicApplicationTest.php
@@ -22,24 +22,58 @@ class BasicApplicationTest extends WebTestCase
     {
         $urls = [
             ['/'],
-            ['/strona-glowna', '/'],
+            ['/en'],
+
             ['/o-stronie'],
+            ['/about'],
+
             ['/regulamin'],
+
             ['/kontakt'],
+            ['/contact'],
+
             ['/wszystkie-wykazy'],
+            ['/all-lists'],
+
             ['/wszystkie-wykazy/1'],
+            ['/all-lists/1'],
+
             ['/wszystkie-wykazy/2'],
+            ['/all-lists/2'],
+
             ['/wszystkie-wykazy/3'],
+            ['/all-lists/3'],
+
+            ['/logowanie'],
+            ['/login'],
+
+            ['/rejestracja'],
+            ['/register'],
+
+            ['/wykaz/1'],
+            ['/list/1'],
+
+            ['/profil/test_user'],
+            ['/profile/test_user'],
+
+            ['/szukaj?s=test'],
+            ['/search?s=test'],
+        ];
+
+        foreach ($urls as $data) {
+            yield $data[0] => $data;
+        }
+    }
+
+    static public function rlv2PublicUrlsProvider(): iterable
+    {
+        $urls = [
+            ['/strona-glowna', '/'],
             ['/wszystkie-wykazy?a=1', '/wszystkie-wykazy'],
             ['/wszystkie-wykazy?a=2', '/wszystkie-wykazy/2'],
             ['/wszystkie-wykazy?a=3', '/wszystkie-wykazy/3'],
-            ['/logowanie'],
-            ['/rejestracja'],
-            ['/wykaz/1'],
             ['/wykaz?id=1', '/wykaz/1'],
-            ['/profil/test_user'],
             ['/profil?u=test_user', '/profil/test_user'],
-            ['/szukaj?s=test'],
         ];
 
         foreach ($urls as $data) {
@@ -51,17 +85,40 @@ class BasicApplicationTest extends WebTestCase
     {
         $urls = [
             ['/utworz-wykaz'],
+            ['/create-list'],
+
             ['/moje-wykazy'],
+            ['/my-lists'],
+
             ['/ustawienia-konta'],
+            ['/account-settings'],
+
             ['/wyloguj', 'http://localhost/'],
+            ['/logout', 'http://localhost/en'],
+
             ['/wykaz/1/dodaj-stacje'],
+            ['/list/1/add-station'],
+
             ['/wykaz/1/edytuj-stacje/1'],
+            ['/list/1/edit-station/1'],
+
             ['/wykaz/1/kopiuj-stacje/1'],
+            ['/list/1/copy-station/1'],
+
             ['/wykaz/1/usun-stacje/1', '/wykaz/1', 'POST'],
+            ['/list/1/delete-station/1', '/list/1', 'POST'],
+
             ['/wykaz/1/ustawienia'],
+            ['/list/1/settings'],
+
             ['/wykaz/1/kolumny'],
+            ['/list/1/columns'],
+
             ['/wykaz/1/eksport'],
+            ['/list/1/export'],
+
             ['/wykaz/1/usun', '/moje-wykazy', 'POST'],
+            ['/list/1/delete', '/my-lists', 'POST'],
         ];
 
         foreach ($urls as $data) {
@@ -70,6 +127,7 @@ class BasicApplicationTest extends WebTestCase
     }
 
     #[DataProvider('publicUrlsProvider')]
+    #[DataProvider('rlv2PublicUrlsProvider')]
     public function test_public_page_seems_to_be_working(string $url, ?string $redirectUrl = null, string $method = 'GET'): void
     {
         $this->client->request($method, $url);
