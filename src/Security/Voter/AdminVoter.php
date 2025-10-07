0cdb6d2b3e5a784d871ef7351e784bb022aa5d80
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2025-10-07 14:06:59 +0200

    Simplify AdminVoter

--- src/Security/Voter/AdminVoter.php
+++ src/Security/Voter/AdminVoter.php
@@ -2,16 +2,16 @@
 
 namespace App\Security\Voter;
 
-use App\Entity\Enum\RadioTable\Status;
 use App\Entity\RadioTable;
 use App\Entity\User;
 use App\Event\AdminRestrictedAccessEvent;
-use RuntimeException;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Vote;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
+#[AutoconfigureTag('security.voter', ['priority' => -1])]
 class AdminVoter extends Voter
 {
     public function __construct(private EventDispatcherInterface $eventDispatcher) {}
@@ -30,34 +30,16 @@ class AdminVoter extends Voter
 
     protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token, ?Vote $vote = null): bool
     {
-        $currentUser = $token->getUser();
+        $user = $token->getUser();
 
-        if (false === $currentUser instanceof User || false === in_array('ROLE_ADMIN', $currentUser->getRoles())) {
-            return false;
-        }
-
-        if ($subject instanceof User) {
-            $user = $subject;
-
-            $result = (false === $user->getPublicProfile() && $user !== $currentUser);
-        }
-        elseif ($subject instanceof RadioTable) {
-            $radioTable = $subject;
-
-            $result = (Status::PRIVATE === $radioTable->getStatus()
-                       && $radioTable->getOwner() !== $currentUser);
-        }
-        else {
-            throw new RuntimeException;
-        }
-
-        // This method could be shorten to just `return Security::isGranted('ROLE_ADMIN')`
-        // but this whole logic is needed to show flash message when site administrator
-        // browses users private contents. Take a look at AdminFlashMessage class.
-        if ($result) {
+        if ($user && in_array('ROLE_ADMIN', $user->getRoles())) {
+            // Thanks to "-1" priority in "security.voter" tag
+            // this event is only dispatched if other voters denied access.
             $this->eventDispatcher->dispatch(new AdminRestrictedAccessEvent);
+
+            return true;
         }
 
-        return $result;
+        return false;
     }
 }
